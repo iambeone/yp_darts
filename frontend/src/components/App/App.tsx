88817105de22b00b08b24e449be02f31e90cbf77
Aleksey Rudometov <alexey@seatti.co>
2022-12-18 21:39:26 +0300

    Remove subrepo

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Start App</h1>
+    </div>
+  );
+}
+
+export default App;
