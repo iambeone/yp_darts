98378dbc2a5ddce3337326939b52e90a2a8b62e2
FabioTw <e.cherwoo@gmail.com>
2022-12-19 19:38:27 +0300

    fix: after review

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import RadioOption from "../RadioOption/Radio-Option";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Start App</h1>
-      <RadioOption
-        name="Пол"
-        values={["Мужской", "Женский", "Оно"]}
-        isRequired
-      />
     </div>
   );
 }
