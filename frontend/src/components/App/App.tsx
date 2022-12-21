b4867e267e19901c9b5e809cc1820942e31dff80
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2022-12-21 03:05:55 +0300

    feat: input text

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import InputText from "../InputText/Input-Text";
 import styles from "./App.module.css";
 
 function App() {
+  // const [name, setName] = React.useState("");
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setName(event.target.value);
+  // };
+
   return (
     <div className={styles.App}>
       <h1>Start App</h1>
+      <InputText
+        required
+        placeholder="fullWide"
+        label="full"
+        size="medium"
+        rows={2}
+        // value={name}
+        // onChange={handleChange}
+        name="phone"
+      />
     </div>
   );
 }
