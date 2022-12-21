a9d1545b2badeeda9bb5ed42730fa376f374e7bf
Apple <IIKolchin@yandex.ru>
2022-12-21 19:41:09 +0300

    sidebar desctop

--- frontend/src/components/tabs/tabs.tsx
+++ frontend/src/components/tabs/tabs.tsx
@@ -1,40 +1,67 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from "react";
-import TournamentActive from "../../images/Vector.svg";
+import TournamentActive from "../../images/tournament-active.svg";
+import Tournament from "../../images/tournament.svg";
+import PlayersActive from "../../images/players-active.svg";
+import Players from "../../images/players.svg";
+import ProtocolsActive from "../../images/protocols-active.svg";
+import Protocols from "../../images/protocols.svg";
+import SettingsActive from "../../images/settings-active.svg";
+import Settings from "../../images/settings.svg";
+import styles from "./tabs.module.css";
 
 const types = [
   {
     title: "Турниры",
-    img: TournamentActive,
+    img: Tournament,
+    imgActive: TournamentActive,
   },
   {
     title: "Игроки",
-    img: TournamentActive,
+    img: Players,
+    imgActive: PlayersActive,
   },
   {
     title: "Протоколы",
-    img: TournamentActive,
+    img: Protocols,
+    imgActive: ProtocolsActive,
+  },
+  {
+    title: "Настройки",
+    img: Settings,
+    imgActive: SettingsActive,
   },
 ];
+
 function Tabs() {
   const [active, setActive] = useState(types[0]);
   return (
-    <>
-      <ul>
-        {types.map((type) => (
-          <li
-            key={type.title}
-            // active={active === type}
-            onKeyPress={() => setActive(type)}
-            onClick={() => setActive(type)}
+    <ul className={styles.ul}>
+      {types.map((type) => (
+        <li
+          key={type.title}
+          className={styles.li}
+          onKeyPress={() => setActive(type)}
+          onClick={() => setActive(type)}
+        >
+          {active === type && <div className={styles.border} />}
+          <img
+            className={styles.img}
+            src={active === type ? type.imgActive : type.img}
+            alt="Иконка"
+            style={{ paddingLeft: active !== type ? "4px" : "0" }}
+          />
+          <p
+            className={styles.title}
+            style={{
+              color: active === type ? "#FFF" : "rgba(255, 255, 255, 0.5)",
+            }}
           >
-            <img src={type.img} alt="Иконка" />
-            <p>{type.title}</p>
-          </li>
-        ))}
-      </ul>
-      <p>{active.title}</p>
-    </>
+            {type.title}
+          </p>
+        </li>
+      ))}
+    </ul>
   );
 }
 export default Tabs;
