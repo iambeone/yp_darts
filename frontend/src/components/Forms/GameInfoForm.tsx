17e219e6d5cd3654afd7ffca24a84cfaffd47a6c
Apple <IIKolchin@yandex.ru>
2023-01-08 21:59:47 +0300

    add label

--- frontend/src/components/Forms/GameInfoForm.tsx
+++ frontend/src/components/Forms/GameInfoForm.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { subjectRF } from "../../utils/constants";
-// import DateTextField from "../DateTextField/DateTextField";
+import DateTextField from "../DateTextField/DateTextField";
 import InputText from "../InputText/Input-Text";
+// import RadioButtons from '../RadioButtons/RadioButtons';
 // import PageTitle from "../PageTitle/PageTitle";
 import SelectOptions from "../SelectOption/SelectOption";
 
@@ -10,8 +11,19 @@ const Section = styled.div`
   margin: 24px;
 `;
 
+const Label = styled.p`
+  margin: 0 0 4px 0;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 143%;
+  letter-spacing: 0.17px;
+  color: rgba(0, 0, 0, 0.87);
+`;
+
 const Caption = styled.p`
-  margin: 3px 0 0 12px;
+  margin: 3px 0 24px 12px;
   font-family: "Roboto";
   font-style: normal;
   font-weight: 400;
@@ -28,13 +40,23 @@ const Group = styled.div`
 export default function GameInfoForm() {
   return (
     <Section>
-      <SelectOptions options={subjectRF} inputWidth={390} />
+      <SelectOptions options={subjectRF} inputWidth={390} label="Субъект РФ" />
       <Caption>Регион, за который играет спортсмен</Caption>
+
       <Group>
-        {/* <SelectOptions label="Спортивный разряд" options={subjectRF} /> */}
-        {/* <DateTextField /> */}
+        <div>
+          <SelectOptions
+            options={subjectRF}
+            inputWidth={308}
+            label="Спортивный разряд"
+          />
+        </div>
+        <DateTextField name="Присвоен" />
       </Group>
+      <Label>ФИО тренера</Label>
       <InputText />
+      <Label>Ведущая рука</Label>
+      {/* <RadioButtons /> */}
     </Section>
   );
 }
--- frontend/src/components/SelectOption/SelectOption.tsx
+++ frontend/src/components/SelectOption/SelectOption.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
@@ -21,14 +22,27 @@ type TOptions = {
 };
 
 type TSelect = {
+  label?: string;
   options: TOptions[];
   inputWidth: number;
 };
 
+const Label = styled.p`
+  margin: 0 0 4px 0;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 143%;
+  letter-spacing: 0.17px;
+  color: rgba(0, 0, 0, 0.87);
+`;
+
 export default function SelectOption(props: TSelect) {
-  const { options, inputWidth } = props;
+  const { label, options, inputWidth } = props;
   return (
     <Stack sx={{ width: inputWidth }}>
+      <Label>{label}</Label>
       <Autocomplete
         onChange={(event, value) => console.log(value)}
         id="tags-outlined"
@@ -41,3 +55,7 @@ export default function SelectOption(props: TSelect) {
     </Stack>
   );
 }
+
+SelectOption.defaultProps = {
+  label: null,
+};
